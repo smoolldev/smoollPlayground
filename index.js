e41b3368d22c41078fbe33a4e58923758cf04c7b
marcel-zdziechowicz <112772290+marcel-zdziechowicz@users.noreply.github.com>
2024-11-25 19:45:34 +0100

    Chore: init
    Signed-off-by: marcel-zdziechowicz <112772290+marcel-zdziechowicz@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,32 @@
+const editors_html_textarea = document.getElementById("editors_html_textarea");
+const editors_css_textarea = document.getElementById("editors_css_textarea");
+const editors_js_textarea = document.getElementById("editors_js_textarea");
+const preview = document.querySelector(".preview");
+const style = document.createElement("style");
+document.head.appendChild(style);
+
+if (!preview) {
+    throw "Ligma ballz!";
+}
+if (!editors_html_textarea) {
+    throw "Ligma ballz!";
+}
+if (!editors_css_textarea) {
+    throw "Ligma ballz!";
+}
+if (!editors_js_textarea) {
+    throw "Ligma ballz!";
+}
+
+editors_html_textarea.addEventListener('change', () => {
+    preview.innerHTML = editors_html_textarea.value;
+});
+
+editors_css_textarea.addEventListener('change', () => {
+    style.textContent = editors_css_textarea.value;
+});
+
+editors_js_textarea.addEventListener('change', () => {
+    const CodeToExecute = new Function(editors_js_textarea.value);
+    CodeToExecute();
+});
